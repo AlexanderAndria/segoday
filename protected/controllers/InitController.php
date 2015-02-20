c9d2892dfb108b08729f776f680d476c77601d89
andatr <andatr2014@yandex.ru>
2015-02-20 17:49:11 +0300

    Подключение к сайту через curl

--- protected/controllers/InitController.php
+++ protected/controllers/InitController.php
@@ -4,8 +4,8 @@ class InitController extends Controller
 	public function actionIndex()
 	{
 		$parser = 	Yii::app()->mparser;
-		$parser->setContent('http://www.ntv.ru/programma/schedule.jsp?bcast=1&channel=1');
-		echo $parser->regContent();
+		$parser->setContent('http://www.1tv.ru/one/shed_ld/z0/?p_r=93120');
+		echo $parser->regexpContent();
 	}
 }
 
--- protected/extensions/parser/MParser.php
+++ protected/extensions/parser/MParser.php
@@ -1,51 +1,66 @@
 <?php
+
 Yii::import('application.extensions.parser.*');
 require_once('simple_html_dom.php');
-		
+
 class MParser extends CApplicationComponent
 {
-	private $content;
-	private $error;
-	
-	public function init()
-	{
-		parent::init();
-	}
-	
-	public function setContent($url)
-	{
-		$this->content = file_get_html($url);
 
-	}
-	
-	public function regContent($kat1='', $kat2='',$kat3='')
-	{
-		if($this->content)
-		{
-			
-			foreach($this->content->find('a') as $element)
-			{
-				$str =  $element->plaintext; 
-				$ert = utf8_decode($str);
-				print_r();
-			}
-		}
-		else 
-		{
-			$this->error[] = 'content  пустой components/MParser.php';
-		}
+    private $content;
+    private $error;
 
-	}
-	
-	public function getContent()
-	{
-		return $this->content;
-	}
-	
-	public function getError()
-	{
-		return $this->error;
-	}
-	
-	
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function setContent($url)
+    {
+       $ch = curl_init($url);
+		curl_setopt($ch,  CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; pl; rv:1.9) Gecko/2008052906 Firefox/3.0'); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_COOKIEFILE, '');
+
+		$str = curl_exec($ch);
+		
+		curl_close($ch);
+        $this->content=str_get_html($str);
+    }
+
+    public function regexpContent($kat1='', $kat2='', $kat3='')
+    {
+        if ($this->content)
+        {
+            foreach ($this->content->find('a') as $element)
+            {
+                $str=$element->plaintext;
+
+                $str=iconv('cp1251', 'UTF-8', $str);
+                $blFind=preg_match('#Ш#i', $str);
+                if ($blFind)
+                {
+                    echo $str;
+                }
+                print_r();
+            }
+        }
+        else
+        {
+            $this->error[]='content  пустой components/MParser.php';
+        }
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function getError()
+    {
+        return $this->error;
+    }
+
 }
